bfd5cb87a8a3db8fed88ed0d6f00ce33bfac0ec2
diih <dvinicius0@gmail.com>
2020-07-08 10:27:02 -0300

    Including the raw project in the repository

---
+++ src/main/java/com/springboot/simple_springboot_book_api/SimpleSpringbootBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.simple_springboot_book_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/simple_springboot_book_api/SimpleSpringbootBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.simple_springboot_book_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
